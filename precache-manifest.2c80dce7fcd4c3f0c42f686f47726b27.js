9bc2b0a6a3da777f2f22cea919da809660608de3
Jakob Kellerer <46808058+erbwort@users.noreply.github.com>
2020-04-23 16:34:55 +0200

    Add files via upload

---
+++ precache-manifest.2c80dce7fcd4c3f0c42f686f47726b27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a71f71ef83114e2af036ac9a758fd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "611da394c5dd88eee3dd",
+    "url": "/static/css/main.70c2e511.chunk.css"
+  },
+  {
+    "revision": "546d2e0c4c401feaec88",
+    "url": "/static/js/2.497369e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.497369e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611da394c5dd88eee3dd",
+    "url": "/static/js/main.4f3ed47e.chunk.js"
+  },
+  {
+    "revision": "765a596ee79fd6955558",
+    "url": "/static/js/runtime-main.7ac4c43c.js"
+  }
+]);
